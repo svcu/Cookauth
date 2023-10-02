af1f9da23b9d864e5bb6a8b14fc32fa53c21a849
svcu2310 <svcu2310@gmail.com>
2023-10-02 15:33:01 -0400

    Moved isCreated and isValidCookie to utils file

--- routes/users/userHandler.js
+++ routes/users/userHandler.js
@@ -1,45 +1,6 @@
 const user = require("../../models/user")
-const aes = require("node-aes")
-
-
-async function isCreated(username, email){
-
-    const verif1 = await user.findOne({username: username});
-    const verif2 = await user.findOne({email: email});
-
-    if(verif1 || verif2){
-        if(verif1){
-            return verif1
-        }else{
-            return verif2
-        }
-    }
-    return null;
-
-}
-
-async function isValidCookie(cookie){
-
-    const dCookie = await aes.decrypt(cookie, process.env.SECRET);
-        
-    const dCookieBody = JSON.parse(dCookie)
-
-    const verif = await isCreated(dCookieBody.username, "");
-    
-    if(verif){
-
-        const verifPwd = await verif.verifyPassword(dCookieBody.password, verif.password);
-
-        if(verifPwd){
-            return true
-        }else{
-            return false
-        }
-
-    }else{
-        return false
-    }
-}
+const aes = require("node-aes");
+const { isCreated, isValidCookie } = require("../../utils/user");
 
 const createUser = async(req, res)=>{
 
